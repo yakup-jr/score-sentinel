04f4288e2c40775f40c69019b64c5c5c2c1c34f4
Dmitry <71725152+yakup-jr@users.noreply.github.com>
2023-08-13 11:25:04 +0400

    SS-4 test(portal*round): add test for round module
    Here was added repositories and services tests for round module. Tests
    with Specification incorrect. In future should be fix these test

---
+++ portal/src/test/java/com/ss/portal/shared/repository/SharedRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.ss.portal.shared.repository;
+
+import com.ss.portal.team.entity.TeamEntity;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.jpa.domain.Specification;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@DisplayName("Specs")
+class SpecsTest {
+
+    @Test
+    @DisplayName("Should return the correct specification when the user ID is provided")
+    void byUserIdWhenUserIdIsProvided() {
+        Long userId = 1L;
+
+        Specification<Object> specification = SharedRepository.Specs.byUserId(userId);
+
+        assertNotNull(specification);
+
+        CriteriaBuilder criteriaBuilder = mock(CriteriaBuilder.class);
+        CriteriaQuery<Object> criteriaQuery = mock(CriteriaQuery.class);
+        Root<Object> root = mock(Root.class);
+
+        Predicate predicate =
+            specification.toPredicate(root, criteriaQuery, criteriaBuilder);
+
+        assertNotNull(predicate);
+        verify(root, times(1)).join("users");
+        verify(criteriaBuilder, times(1)).equal(any(), eq(userId));
+    }
+
+    @Test
+    @DisplayName("Should throw an exception when the teamId is null")
+    void byTeamIdWhenTeamIdIsNullThenThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SharedRepository.Specs.byTeamId(null);
+        });
+    }
+
+    @Test
+    @DisplayName("Should return the correct specification when the teamId is provided")
+    void byTeamIdWhenTeamIdIsProvided() {
+        Long teamId = 1L;
+        Specification<TeamEntity> expectedSpecification = (root, query, builder) -> {
+            var teamJoin = root.join("teams");
+            return builder.equal(teamJoin.get("id"), teamId);
+        };
+
+        Specification<TeamEntity> actualSpecification =
+            SharedRepository.Specs.byTeamId(teamId);
+
+        assertEquals(expectedSpecification, actualSpecification);
+    }
+}
